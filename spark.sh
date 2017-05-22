08f84846d020981fcbfad0a2f85c04ba016a32cf
Tom Lant <lampholder@gmail.com>
2017-05-22 12:38:37 +0000

    temporary hack to restore missing dependency

--- spark.sh
+++ spark.sh
@@ -11,20 +11,28 @@ git clone https://github.com/vector-im/riot-web.git
 git clone https://github.com/matrix-org/matrix-react-sdk.git
 git clone https://github.com/matrix-org/matrix-js-sdk.git
 
-mkdir riot-web/node_modules
-mkdir matrix-react-sdk/node_modules
-ln -rs matrix-js-sdk riot-web/node_modules/matrix-js-sdk
-ln -rs matrix-js-sdk matrix-react-sdk/node_modules/matrix-js-sdk
-ln -rs matrix-react-sdk riot-web/node_modules/matrix-react-sdk
-
 (cd riot-web && git checkout $branch)
 (cd matrix-react-sdk && git checkout $branch)
 (cd matrix-js-sdk && git checkout $branch || git checkout develop)
 
+(cd riot-web && npm install)
+(cd matrix-react-sdk && npm install)
+(cd matrix-js-sdk && npm install)
+
+rm -rf riot-web/node_modules/matrix-js-sdk
+rm -rf riot-web/node_modules/matrix-react-sdk
+rm -rf matrix-react-sdk/node_modules/matrix-js-sdk
+
+(cd riot-web/node_modules && ln -s ../../matrix-react-sdk && ln -s ../../matrix-js-sdk)
+(cd matrix-react-sdk/node_modules && ln -s ../../matrix-js-sdk)
+
 # Setup the thing with the sample config
 (cd riot-web && cp config.sample.json config.json)
 
+# Temporary hack
+(cd matrix-js-sdk && npm i source-map-loader)
+
 # Build it all properly:
-(cd matrix-js-sdk && npm install && npm run build)
-(cd matrix-react-sdk && npm install npm run build)
-(cd riot-web && npm install && npm run build)
+(cd matrix-js-sdk && npm run build)
+(cd matrix-react-sdk && npm run build)
+(cd riot-web && npm run build)
