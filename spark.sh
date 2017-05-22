b3dca510b5514617c624177787a1b90595d229a0
Tom Lant <lampholder@gmail.com>
2017-05-22 11:30:43 +0000

    some fixes

--- spark.sh
+++ spark.sh
@@ -11,21 +11,15 @@ git clone https://github.com/vector-im/riot-web.git
 git clone https://github.com/matrix-org/matrix-react-sdk.git
 git clone https://github.com/matrix-org/matrix-js-sdk.git
 
+mkdir riot-web/node_modules
+mkdir matrix-react-sdk/node_modules
+ln -rs matrix-js-sdk riot-web/node_modules/matrix-js-sdk
+ln -rs matrix-js-sdk matrix-react-sdk/node_modules/matrix-js-sdk
+ln -rs matrix-react-sdk riot-web/node_modules/matrix-react-sdk
+
 (cd riot-web && git checkout $branch)
 (cd matrix-react-sdk && git checkout $branch)
 (cd matrix-js-sdk && git checkout $branch || git checkout develop)
-
-(cd riot-web && npm install)
-(cd matrix-react-sdk && npm install)
-
-rm -rf riot-web/node_modules/matrix-react-sdk
-ln -s matrix-react-sdk riot-web/node_modules/matrix-react-sdk
-
-# Put the fresh version of matrix-{react,js}-sdk where they need to be
-rm -rf riot-web/node_modules/matrix-js-sdk
-rm -rf matrix-react-sdk/node_modules/matrix-js-sdk
-ln -s matrix-js-sdk riot-web/node_modules/matrix-js-sdk
-ln -s matrix-js-sdk matrix-react-sdk/node_modules/matrix-js-sdk
 
 # Setup the thing with the sample config
 (cd riot-web && cp config.sample.json config.json)
